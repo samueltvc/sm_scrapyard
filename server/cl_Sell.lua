750f855de622d3cdcf2e629e9a9eea28d5fd549a
samueltv_ <140980636+samueltvc@users.noreply.github.com>
2023-09-22 15:10:00 +0200

    Add files via upload

--- server/cl_Sell.lua
+++ server/cl_Sell.lua
@@ -1,18 +1,18 @@
------------------For support, scripts, and more----------------
---------------- https://discord.gg/wasabiscripts  -------------
----------------------------------------------------------------
-
 ESX = exports["es_extended"]:getSharedObject()
 
 lib.callback.register('ws_sellshop:sellItem', function(source, data)
     local xPlayer = ESX.GetPlayerFromId(source)
-    local xItem = xPlayer.getInventoryItem(data.item)
-    if xItem.count < data.quantity then
-        return false
-    else
-        local profit = math.floor(data.price * data.quantity)
-        xPlayer.removeInventoryItem(data.item, data.quantity)
+    local item = data.item
+    local quantity = data.quantity
+
+    local xItem = xPlayer.getInventoryItem(item)
+
+    if xItem.count >= quantity then
+        local profit = math.floor(data.price * quantity)
+        xPlayer.removeInventoryItem(item, quantity)
         xPlayer.addAccountMoney(data.currency, profit)
         return profit
     end
+
+    return false
 end)
