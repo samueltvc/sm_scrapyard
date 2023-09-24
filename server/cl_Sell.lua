7f89c1666ed4964da37a4c3a7ebd5098120d7a4f
samueltv_ <140980636+samueltvc@users.noreply.github.com>
2023-09-24 18:11:59 +0200

    Update cl_Sell.lua

--- server/cl_Sell.lua
+++ server/cl_Sell.lua
@@ -1,18 +1,1 @@
-ESX = exports["es_extended"]:getSharedObject()
 
-lib.callback.register('ws_sellshop:sellItem', function(source, data)
-    local xPlayer = ESX.GetPlayerFromId(source)
-    local item = data.item
-    local quantity = data.quantity
-
-    local xItem = xPlayer.getInventoryItem(item)
-
-    if xItem.count >= quantity then
-        local profit = math.floor(data.price * quantity)
-        xPlayer.removeInventoryItem(item, quantity)
-        xPlayer.addAccountMoney(data.currency, profit)
-        return profit
-    end
-
-    return false
-end)
